27ac341e7f2873b95ce466fce775e6a31c9745cd
ChikamsoChidi <109491366+ChikamsoChidi@users.noreply.github.com>
2024-12-16 17:40:13 +0100

    Update Curriculum-Vitae.py

--- Curriculum-Vitae.py
+++ Curriculum-Vitae.py
@@ -6,7 +6,7 @@ current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 
 css_file = current_dir/"styles"/"main.css"
 resume_file = current_dir/"assets"/"cv.pdf"
-profile_pic = current_dir/"Assets"/"profile-pic.png"
+profile_pic = current_dir/"assets"/"profile-pic.png"
 
 
 # --- GENERAL SETTINGS ---
